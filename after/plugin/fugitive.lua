a5bdc097590f96f2e7d7d0b77b27d46283a9a230
Benjamin Krenn <benjamin.krenn@leftshift.one>
2023-05-16 08:41:45 +0200

    modified mappings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,3 @@
 -- fugitive related mappings
-vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "git"})
-vim.keymap.set("n", "<leader>gp", vim.cmd.GitPush, { desc = "git"})
+vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "git" })
+vim.keymap.set("n", "<leader>gp", function() vim.cmd("Git push") end, { desc = "git push" })
